55792878845922e2f4835aa151a5512dbd2849da
Cubsfan347 <dkozar@students.mchenry.edu>
2026-08-26 10:33:25 -0500

    Add files via upload

---
+++ joke.py
@@ -0,0 +1,4 @@
+print("Why was the Ant confused?")
+print("")
+print("Because all of his uncles were Ants")
+# this is a corny joke
---
+++ madlibs.py
@@ -0,0 +1,25 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included (Copy and paste THIS comment from ).
+[ ] 2. Program asks for at least 5 different inputs (variables).
+[ ] 3. Output uses F-Strings to combine text and variables.
+[ ] 4. Output uses at least one escape sequence (\n or \t).
+[ ] 5. Code contains comments explaining the steps.
+[ ] 6. Program runs without errors.
+-----------------------------------------------------------------------
+"""
+
+# ℹ️input
+emotion = input("please enter an emotion:happy ")
+special_day = input("please enter special day:birthday ")
+name = input("please enter name:Walter ")
+pronoun = input("please enter pronoun:you ")
+preposition = input("please enter preposition:to")
+
+# ℹ️output happy birthday
+print(f"{emotion} {special_day} {preposition} {pronoun}")
+print(f"{emotion} {special_day} {preposition} {pronoun}")
+print(f"{emotion} {special_day} dear {name}")
+print(f"{emotion} {special_day} {preposition} {pronoun}")
